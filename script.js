86b195ee492f516234930b8fdf7e742a0fd8e680
kendy-karakawa <kendy.karakawa@gmail.com>
2022-10-15 08:20:05 -0300

    primeiro comit

---
+++ script.js
@@ -0,0 +1,99 @@
+function addBordaP1() {
+  const borda = document.querySelector(".p1");
+  borda.classList.toggle("ativo");
+}
+
+function addBordaP2() {
+  const borda = document.querySelector(".p2");
+  borda.classList.toggle("ativo");
+}
+
+function addBordaP3() {
+  const borda = document.querySelector(".p3");
+  borda.classList.toggle("ativo");
+}
+
+function addBordaP4() {
+  const borda = document.querySelector(".p4");
+  borda.classList.toggle("ativo");
+}
+
+function addBordaP5() {
+  const borda = document.querySelector(".p5");
+  borda.classList.toggle("ativo");
+}
+
+function addBordaB1() {
+  const borda = document.querySelector(".b1");
+  borda.classList.toggle("ativo");
+}
+
+function addBordaB2() {
+  const borda = document.querySelector(".b2");
+  borda.classList.toggle("ativo");
+}
+
+function addBordaB3() {
+  const borda = document.querySelector(".b3");
+  borda.classList.toggle("ativo");
+}
+
+function addBordaB4() {
+  const borda = document.querySelector(".b4");
+  borda.classList.toggle("ativo");
+}
+
+function addBordaB5() {
+  const borda = document.querySelector(".b5");
+  borda.classList.toggle("ativo");
+}
+
+function addBordaS1() {
+  const borda = document.querySelector(".s1");
+  borda.classList.toggle("ativo");
+}
+
+function addBordaS2() {
+  const borda = document.querySelector(".s2");
+  borda.classList.toggle("ativo");
+}
+
+function addBordaS3() {
+  const borda = document.querySelector(".s3");
+  borda.classList.toggle("ativo");
+}
+
+function addBordaS4() {
+  const borda = document.querySelector(".s4");
+  borda.classList.toggle("ativo");
+}
+
+function addBordaS5() {
+  const borda = document.querySelector(".s5");
+  borda.classList.toggle("ativo");
+}
+
+function containerConfirma() {
+  const container = document.querySelector(".container-confirma");
+  container.classList.remove("escondido");
+}
+
+function cancelar() {
+  const cancel = document.querySelector(".container-confirma");
+  cancel.classList.add("escondido");
+}
+
+function WhatsApp() {
+  /*
+`Olá, gostaria de fazer o pedido:
+- Prato: ${Frango Yin Yang}
+- Bebida: ${Coquinha Gelada}
+- Sobremesa: ${Pudim}
+Total: R$ ${27.70}
+
+Nome: ${Fulano}
+Endereço: ${Rua...}`
+
+ https://wa.me/5511987641124?text=
+*/
+}
